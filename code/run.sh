0cdb9a3f9054323c1883059c7e653b139b444052
Souhaib Ben Taieb <bsouhaib@gmail.com>
2018-03-08 12:06:43 +1100

    run

---
+++ code/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+rscript="main.R"
+njobs=20
+
+towardspbu=TRUE
+lambdasel="min"
+nbsimul=20
+
+
+allidjobs=$(seq 1 $njobs )
+
+for idjob in ${allidjobs[@]}
+do
+
+flag="$idjob-$towardspbu-$lambdasel"
+    
+  Rscript --vanilla $rscript $idjob $towardspbu $lambdasel $nbsimul > "/home/rstudio/sparseHTS/rout/results-$flag.Rout" 2> "/home/rstudio/sparseHTS/rout/errorFile-$flag.Rout" &
+
+done
+
